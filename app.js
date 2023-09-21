8c2d2d7b1524826cced629e4796cfbaeddfa6dad
Ali <aliprg0@gmail.com>
2023-09-21 23:51:26 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const haumberger = document.querySelector("#haumberger")
+const menu = document.querySelector("#menu")
+
+haumberger.addEventListener("click",()=>{
+    menu.classList.toggle("hidden")
+})
